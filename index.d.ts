85befeb5e8b5d4ed6b2322abdc383e2ad47f6b0c
Philipp Alker <philipp.alker@citynode.de>
2023-11-03 09:30:56 +0100

    fix: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,28 @@
+import { AzDpsClient } from "./types/AzDpsClient";
+import { AzIoTHubClient } from "./types/AzIoTHubClient";
+
 export type ConnectionInfo = {
-    scopeId: string,
-    deviceId: string,
-    hubName: string,
-    deviceKey: string,
-    masterKey?: string,
-    modelId?: string,
-    status: string,
-    connected: bool
-}
+  scopeId: string;
+  deviceId: string;
+  hubName: string;
+  deviceKey: string;
+  masterKey?: string;
+  modelId?: string;
+  status: string;
+  connected: bool;
+};
 
 export type DeviceTwin = {
-    reported: any,
-    desired: any
-}
+  reported: any;
+  desired: any;
+};
 
 export type CommandInfo = {
-    method: string,
-    payload: string,
-    response: string,
-    rid: number,
-    dirty: boolean
-}
+  method: string;
+  payload: string;
+  response: string;
+  rid: number;
+  dirty: boolean;
+};
+
+export { AzIoTHubClient, AzDpsClient };
