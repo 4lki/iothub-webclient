2e35b4222fe8a4615135d134c31e129f79882a39
Philipp Alker <philipp.alker@citynode.de>
2023-11-03 20:27:55 +0100

    jo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,26 @@
 export * from "./AzDpsClient";
 export * from "./AzIoTHubClient";
+
+export interface ConnectionInfo {
+  scopeId: string;
+  deviceId: string;
+  hubName: string;
+  deviceKey: string;
+  masterKey?: string;
+  modelId?: string;
+  status: string;
+  connected: boolean;
+}
+
+export interface DeviceTwin {
+  reported: any;
+  desired: any;
+}
+
+export interface CommandInfo {
+  method: string;
+  payload: string;
+  response: string;
+  rid: number;
+  dirty: boolean;
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,26 +1,23 @@
 export * from "./AzDpsClient";
 export * from "./AzIoTHubClient";
-
-export type ConnectionInfo = {
-  scopeId: string;
-  deviceId: string;
-  hubName: string;
-  deviceKey: string;
-  masterKey?: string;
-  modelId?: string;
-  status: string;
-  connected: bool;
-};
-
-export type DeviceTwin = {
-  reported: any;
-  desired: any;
-};
-
-export type CommandInfo = {
-  method: string;
-  payload: string;
-  response: string;
-  rid: number;
-  dirty: boolean;
-};
+export interface ConnectionInfo {
+    scopeId: string;
+    deviceId: string;
+    hubName: string;
+    deviceKey: string;
+    masterKey?: string;
+    modelId?: string;
+    status: string;
+    connected: boolean;
+}
+export interface DeviceTwin {
+    reported: any;
+    desired: any;
+}
+export interface CommandInfo {
+    method: string;
+    payload: string;
+    response: string;
+    rid: number;
+    dirty: boolean;
+}
